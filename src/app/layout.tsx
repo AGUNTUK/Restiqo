26e9ee6c7769f9cbea050235ca707aa66dade94a
Restiqa Developer <developer@restiqa.com>
2026-03-09 22:33:30 +0600

    Add Google site verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export const metadata: Metadata = {
     },
   },
   verification: {
-    google: 'your-google-verification-code',
+    google: '7ihYkOccgaKwxxVZqTx9d6MrWcQuXIIgILAW2SlbG4o',
   },
 }
 
